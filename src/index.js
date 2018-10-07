2f134f663717190ee681c998d72291754d1256ab
GoldenKit <nikitoska01@gmail.com>
2018-10-07 23:52:01 +0300

    half-tried solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  let arr = [];
+  let ost;
+  let chas;
+  let bufer = number;
+  let string = "";
+  do {
+    ost = bufer % base;
+    chas = Math.floor(bufer / base);
+    arr.push(ost);
+    bufer = chas;
+    } while (chas >= base)
+  arr.push(chas);
+  for (let i = arr.length - 1; i >= 0; i--){
+    string += arr[i];
+    }
+  let num = Number(string);
+ let k = 0;
+ let n = 1;
+ while (Math.floor(num/(Math.pow(5,n))) > 0) {
+   k = k + Math.floor(num/(Math.pow(5,n)));
+   n++;
+ }
+ return k;
+ }
