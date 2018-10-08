8e45532c1c22d8b8448003fe308d8d55c1ebfa64
GoldenKit <nikitoska01@gmail.com>
2018-10-08 19:35:23 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
-module.exports = function getZerosCount(number, base) {
-  let arr = [];
-  let ost;
-  let chas;
-  let bufer = number;
-  let string = "";
-  do {
-    ost = bufer % base;
-    chas = Math.floor(bufer / base);
-    arr.push(ost);
-    bufer = chas;
-    } while (chas >= base)
-  arr.push(chas);
-  for (let i = arr.length - 1; i >= 0; i--){
-    string += arr[i];
-    }
-  let num = Number(string);
- let k = 0;
- let n = 1;
- while (Math.floor(num/(Math.pow(5,n))) > 0) {
-   k = k + Math.floor(num/(Math.pow(5,n)));
-   n++;
- }
- return k;
+module.exports = function getzeros(number, base) {
+  let buferBase = base;
+   let zeros = number;
+   for (let i = 2; i <= buferBase; i++){
+     if (buferBase % i == 0){
+       let count1 = 0;
+       while (buferBase % i == 0){
+         count1++;
+         buferBase = Math.floor(buferBase / i);
+       }
+       let buferNumber = number;
+       let count2 = 0;
+       while (buferNumber / i  > 0){
+         buferNumber = Math.floor(buferNumber / i);
+         count2 += buferNumber;
+       }
+       zeros = Math.min(zeros, Math.floor(count2 / count1));
+     }
+   }
+   return zeros;
  }
